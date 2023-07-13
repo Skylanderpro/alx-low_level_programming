8a4e1bc0d929e9079551f5cf5958f55871f3505a
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-13 08:51:14 +0300

    alx-low_level_programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <stdio.h>
+/**
+ * main - prints
+ *
+ * This file prints programming
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
