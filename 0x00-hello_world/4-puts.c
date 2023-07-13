e113deae897dc7b6e58f29d33aeae09624df36c5
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-13 08:49:38 +0300

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+/**
+ * main - prints
+ *
+ * This file prints programming
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main()
+int main(void)
 {
 	char string1[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string1);
-	return 0;
+	return (0);
 }
