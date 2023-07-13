ecaf3a11ee8b4252e26ebf71765088e7d267fd64
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-13 12:33:48 +0300

    alx-low_level_programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints
+ *
+ *Printing to the standart error
+ *
+ *Return: 1 (success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
