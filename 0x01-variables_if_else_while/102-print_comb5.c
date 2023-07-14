18dcd69a18dc5498f9e369f4d21cdcb4048ff1d3
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-14 15:02:29 +0300

    if else while and variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 		{
 			for (k = i; k < 10; k++)
 			{
-				for (l = j + 1; l < 10; l++)
+				for (l = j; l < 10; l++)
 				{
 					putchar((i % 10) + '0');
 					putchar((j % 10) + '0');
