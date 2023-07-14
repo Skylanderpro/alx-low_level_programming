9723554a396973a8fa7adf12014390eb11214749
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-14 15:00:31 +0300

    if else while and variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *main - prints two two digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = i; k < 10; k++)
+			{
+				for (l = j + 1; l < 10; l++)
+				{
+					putchar((i % 10) + '0');
+					putchar((j % 10) + '0');
+					putchar(' ');
+					putchar((k % 10) + '0');
+					putchar((l % 10) + '0');
+					if (i != 9 || j != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+		return (0);
+}
