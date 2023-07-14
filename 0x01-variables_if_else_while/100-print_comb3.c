2d33814a322e0dcb5fe1f995656b90840fa3f53d
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-14 13:28:24 +0300

    if else while and variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - prints different comb of 2 digits
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
