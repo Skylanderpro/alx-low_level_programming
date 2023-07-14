2ca9645ee58ec3f44eba083d550b0261c196e73d
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-14 12:37:22 +0300

    if else while and variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 to 9
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
