ed69b27a8a39303b4d7fd180a0c6e50ae90a64cb
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-18 16:51:24 +0300

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ *main - prints lowercase alphabets
+ *
+ *Return: void (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
