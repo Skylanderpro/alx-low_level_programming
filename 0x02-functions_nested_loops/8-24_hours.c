63d55e90dadd355d3ca59a988a65579db6a78b45
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-18 20:56:16 +0300

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *jack_bauer - print minutes
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i, l;
+
+	i = 0;
+	while(i < 24)
+	{
+		for (l = 0; l < 60; l++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(58);
+			_putchar(l / 10 + '0');
+			_putchar(l % 10 + '0');
+			_putchar('\n');
+		}
+		i++;
+	}
+}
