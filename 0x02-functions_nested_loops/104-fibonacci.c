f4c3fbbb201db9567393ed71346abb550fe6881d
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-19 02:33:38 +0300

    Functions and nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int count;
-	unsigned long fibo_0 = 0, fibo_1= 1, sum;
+	unsigned long fibo_0 = 0, fibo_1 = 1, sum;
 	unsigned long fib_half_1, fib_half_2, fib1_half_1, fib1_half_2;
 	unsigned long half, half1;
 
@@ -34,7 +34,7 @@ int main(void)
 		}
 		printf("%lu%lu", half, half1);
 		if (count != 98)
-			printf("' ");
+			printf(", ");
 		fib_half_1 = fib1_half_1;
 		fib_half_2 = fib1_half_2;
 		fib1_half_1 = half;
