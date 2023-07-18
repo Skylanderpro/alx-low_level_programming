a1890dd152bb038622800dbc9cacd992e105b90b
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-19 01:54:07 +0300

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - prints fibonacci series
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	long int a = 1, b = 2, result;
+
+	for (i = 0; i < 98; i++)
+	{
+		if (i != 97)
+			printf("%ld, ", a);
+		if (i == 97)
+			printf("%ld\n", a);
+		result = a + b;
+		a = b;
+		b = result;
+	}
+	return (0);
+}
