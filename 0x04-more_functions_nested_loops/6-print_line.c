837f2abfc58368830c0884deba4f883c170bea11
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-20 22:41:11 +0300

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_line - prints _ n times in terminal
+ *@n: number of times _ is printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+			_putchar('\n');
+		}
+	}
+}
