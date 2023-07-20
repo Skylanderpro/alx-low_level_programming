a0503528cbd300fbe28713d8b9495c7e0b65920a
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-20 22:21:56 +0300

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *more_numbers - print 14 numbers 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, k;
+
+	1 = 0;
+
+	while (i < 15)
+	{
+		if (i >= 10)
+			_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+	}
+	_putchar('\n');
+}
