3ff86d0f10bdead2d69300c1b41f4a9bc983ccad
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-21 00:36:55 +0300

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,17 +9,22 @@ void print_triangle(int size)
 {
 	int i, j, num;
 
-	num = size;
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (j >= num || j == num - 1)
-				printf("#");
-			if (j < num - 1)
-				printf(" ");
-		}
-		num--;
+	if (size <= 0)
 		printf("\n");
+	else
+	{
+		num = size;
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j >= num || j == num - 1)
+					printf("#");
+				if (j < num - 1)
+					printf(" ");
+			}
+			num--;
+			printf("\n");
+		}
 	}
 }
