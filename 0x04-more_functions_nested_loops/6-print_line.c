8b4ae821092766cd1cae564818b6ea52f889a10c
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-20 22:45:04 +0300

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,12 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
 			_putchar(95);
-			_putchar('\n');
 		}
 	}
+	_putchar('\n');
 }
