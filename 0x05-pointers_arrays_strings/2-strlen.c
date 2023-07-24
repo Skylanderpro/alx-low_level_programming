93a174049b43934152388e023cf5056f9be8d9af
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-25 00:46:33 +0300

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - returns string length
+ *@s: string to check length
+ *
+ * Return: 0 for success
+ */
+
+int _strlen(char *s)
+{
+	int h;
+
+	h = strlen(*s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,9 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 void reset_to_98(int *n);
 #endif
