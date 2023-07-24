30a7c2107b2987ed22096aa88258a117c7652f72
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-25 00:37:30 +0300

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - set value to 98
+ *@n: argument
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+void reset_to_98(int *n);
+#endif
