ceea59e09076a8d70278cd5aa358a70a20cc06ac
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-25 11:15:52 +0300

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,18 +6,20 @@
  *@src: source
  *Return: str
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
 	int x = 0;
 
 	while (*(src + l) != '\0')
-	{
+	{	
 		l++;
+		
 	}
-	for (; x < l; x++)
-		dest[x] = src[x];
-	dest[l] += '\0';
+	for ( ; x < l ; x++)
+	{	
+		dest[x] = src[x];	
+	}				
+	dest[l] = '\0';	
 	return (dest);
 }
