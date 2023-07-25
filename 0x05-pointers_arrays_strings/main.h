baf107fe7980b8d43cbecce4d73dddf789886b58
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-25 10:57:13 +0300

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
