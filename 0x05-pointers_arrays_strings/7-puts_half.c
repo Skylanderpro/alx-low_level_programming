31a849ca4ab4c98249cebecbd04c7715d2eca610
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-25 10:06:48 +0300

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *puts_half - prints half a string
+ *
+ *@str: the string to print
+ */
+
+void puts_half(char *str)
+{
+	int n, j, lon;
+
+	lon = 0;
+
+	for (n = 0; str[n] != '\0'; n++)
+		lon++;
+	n = (lon / 2);
+
+	if ((lon % 2) == 1)
+		j = ((lon + 1) / 2);
+	for (n = j; str[n] != '\0'; n++)
+		putchar(str[n]);
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+void puts_half(char *str);
 void puts2(char *str);
 void rev_string(char *s);
 void print_rev(char *s);
