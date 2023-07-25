da4f5208a0c2987230a9b426015637a0820fedbd
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-25 10:59:24 +0300

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -28,9 +28,9 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n* 10 + digit;
+			n = n * 10 + digit;
 			f = 1;
-			if (s[i+1] < '0' || s[i + 1] > '9')
+			if (s[i+1] < '0' || s[i  + 1] > '9')
 				break;
 			f = 0;
 		}
