5d57364f606ae91215d83ce1d892158a6f72ccf6
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-28 01:15:13 +0300

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,11 +10,11 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int t;
-	
+
 	for (i = 0; i < n--; i++)
-	{	
-		t = a[i];	
-		a[i] = a[n];			
-		a[n] = t;			
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
 }
