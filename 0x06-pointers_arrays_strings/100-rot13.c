6c274d7c60707488c723eb5050d11d802e142511
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-28 01:54:20 +0300

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,22 +9,24 @@
  */
 
 char *rot13(char *s)
-{	
+{
 	int i;
-	int j;	
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";	
+	int j;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
-			{			
+			{
 				s[i] = datarot[j];
-					break;		
+					break;
 			}
 		}
-	}							
+	}
 	return (s);
 }
 
