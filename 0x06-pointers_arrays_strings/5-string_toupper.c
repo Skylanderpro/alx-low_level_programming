9b822ae1e8997c210fb67663b54ae8b55b55a1fc
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-07-28 01:17:27 +0300

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,12 +9,12 @@ char *string_toupper(char *n)
 {
 	int i;
 
-	i = 0;	
-	while (n[i] != '\0')	
-	{		
-		if (n[i] >= 'a' && n[i] <= 'z')	
-			n[i] = n[i] - 32;	
-		i++;		
-	}	
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
 	return (n);
 }
