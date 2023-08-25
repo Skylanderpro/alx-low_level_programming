2164b9b97b5f5fdc3c03662c44e5ca6dd7a19735
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-25 21:11:21 +0300

    Applied modifications to files: 0x08-recursion/5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -25,9 +25,11 @@ int _sqrt_recursion(int n)
 
 int find_sqrt(int n, int start, int end)
 {
+	int mid;
+
 	if (start > end)
 		return (-1);
-	int mid = (start + end) / 2;
+	mid = (start + end) / 2;
 
 	if (mid * mid == n)
 		return mid; /*natural squareroot found*/
