0f7642c5ce02c3d19f62622a1b5f0ecc80f17e38
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-25 21:06:07 +0300

    Applied modifications to files: 0x08-recursion/5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - returns the square root
+ *@n: number whose sqrt is returned
+ *
+ *Return: the natural sqrt or -1 if not found
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return find_sqrt(n, 0, n);
+}
+
+/**
+ *find_sqrt - second function that finds the squareroot using recursion
+ *@n: the input number
+ *@start: the starting point for finding the sqrt
+ *@end: the ending point for the search
+ *Return: the natural sqrt of n
+ */
+
+int find_sqrt(int n, int start, int end)
+{
+	if (start > end)
+		return (-1);
+	int mid = (start + end) / 2;
+
+	if (mid * mid == n)
+		return mid; /*natural squareroot found*/
+	if (mid * mid > n)
+		return find_sqrt(n, start, mid - 1); /*searching in the lower half*/
+	return find_sqrt(n, mid + 1, end); /* seaching in upper half*/
+}
