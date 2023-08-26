df403fe8dc57d62e187f1f65729a2ebf183e0e46
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-26 22:13:39 +0300

    Applied modifications to files: 0x0A-argc_argv/0-whatsmyname.c 0x0A-argc_argv/mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  *@argv: array of arguments to main in commandline
  *Return: always 0
  */
-int main(argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n"argv[0]);
+	argc = argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
