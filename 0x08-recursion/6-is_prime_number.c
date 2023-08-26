000f7bb8966a4ecf22ee9149e6ae040377547899
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-26 14:51:15 +0300

    Applied modifications to files: 0x08-recursion/100-is_palindrome.c 0x08-recursion/101-wildcmp.c 0x08-recursion/6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
-int prime_finder(int b, int k);
+int actual_prime(int n, int i);
 
 /**
- * is_prime_number - determines if an integer is a prime number or not
+ * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
- * Return: 1 if n is a prime number, 0 o.w
+ * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (prime_finder(n, n - 1));
+	return (actual_prime(n, n - 1));
 }
 
 /**
- * prime_finder - calculates if a number is prime recursively
- * @b: number to check if prime or not
- * @k: value for iteration
- *
- * Return: 1 if n is prime, 0 0.w
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ * Return: 1 if n is prime, 0 if not
  */
-int prime_finder(int b, int k)
+int actual_prime(int n, int i)
 {
-	if (b == 1)
+	if (i == 1)
 		return (1);
-	if (b % k == 0 && k > 0)
+	if (n % i == 0 && i > 0)
 		return (0);
-	return (prime_finder(b, k - 1));
+	return (actual_prime(n, i - 1));
 }
