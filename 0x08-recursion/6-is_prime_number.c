e7dd778687865e7412f8421ad24ebf4c84c0016f
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-26 14:27:49 +0300

    Applied modifications to files: 0x08-recursion/6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,13 +6,15 @@
  */
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
 		return (0); /*0 and 1 are not prime numbers*/
 	if (n <= 3)
 		return (1); /* both 2 and 3 are prime numbers*/
 	if (n % 2 == 0 || n % 3 == 0)
 		return (0); /*numbers divisible by 2 and 3 are not prime*/
-	for (int i = 5; i * i <= n; i += 6)
+	for (i = 5; i * i <= n; i += 6)
 	{
 		if (n % i == 0 || n % (i + 2) == 0)
 			return (0);
