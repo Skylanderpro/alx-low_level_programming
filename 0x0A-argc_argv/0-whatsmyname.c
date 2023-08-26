bafcf02c39bf20b4a405a296cf60799b77983fed
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-26 21:44:36 +0300

    Applied modifications to files: 0x0A-argc_argv/0-whatsmyname.c 0x0A-argc_argv/1-args.c 0x0A-argc_argv/README.md 0x0A-argc_argv/main.h 0x0A-argc_argv/p

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - prints it's name
+ *@argc: number of arguments to main
+ *@argv: array of arguments to main in commandline
+ *Return: always 0
+ */
+int main(argc, char *argv[])
+{
+	printf("%s\n"argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+0-whatsmyname.c Write a program that prints its name, followed by a new line.
+1-args.c Write a program that prints the number of arguments passed into it
