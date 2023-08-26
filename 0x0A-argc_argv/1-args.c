e88f8d2736d31e191bfe446e8356f1a955c74f2d
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-26 21:50:50 +0300

    Applied modifications to files: 0x0A-argc_argv/1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - returns the number of arguments to it
+ *@argc: the number of arguments to main
+ *@argv: the array to arguments to c
+ *Return: Always 0 (success)
+ */
+int main(argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
