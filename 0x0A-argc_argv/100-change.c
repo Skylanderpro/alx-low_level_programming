391f6661647c8a4fff35ba4ef16bd1e45055ea20
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-26 23:55:23 +0300

    Applied modifications to files: 0x0A-argc_argv/100-change.c 0x0A-argc_argv/change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,27 +1,34 @@
 #include "main.h"
-
+/**
+ *main - number of cents calculator for change
+ *@argc: number of arguments to main
+ *@argv: stores the values in main
+ *Return: 0 for success
+ */
 int main(int argc, char *argv[])
 {
+	int i, cents, num_coins, count;
+	int coins[] = {25, 10, 5, 2, 1};
+
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;			
+		return (1);			
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 
 	if (cents < 0)
 	{
 		printf("0\n");
-		return 0;		
+		return (0);		
 	}
 
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(coins) / sizeof(coins[0]);
-	int count = 0;
+	num_coins = sizeof(coins) / sizeof(coins[0]);
+	count = 0;
 
-	for (int i = 0; i < num_coins; i++)
+	for (i = 0; i < num_coins; i++)
 	{
 		count += cents / coins[i];
 		cents %= coins[i];				
@@ -29,5 +36,5 @@ int main(int argc, char *argv[])
 
 	printf("%d\n", count);
 
-	return 0;
+	return (0);
 }
