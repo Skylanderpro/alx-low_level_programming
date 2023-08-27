3030b63ad6dc7f226aace0cb969610fc7a97bb8b
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-27 11:35:21 +0300

    Applied modifications to files: 0x0B-malloc_free/100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,23 +13,23 @@ char *argstostr(int ac, char **av)
 	char *result;
 
 	if (ac == 0 || av == NULL)
-	{		  
-		return NULL;
+	{
+		return (NULL);
 	}
 
 	total_length = 0;
-	    
+
 	for (i = 0; i < ac; i++)
-	{		
+	{
 		total_length += strlen(av[i]) + 1; /* +1 for the newline character*/
 	}
 
 	/* Allocate memory for the concatenated string*/
-	result = (char *)malloc(total_length * sizeof(char));
+	result = (char *)malloc(total_length * sizeof(char) + 1);
 
 	if (result == NULL)
-	{	
-		return NULL; /* Memory allocation failed*/
+	{
+		return (NULL); /* Memory allocation failed*/
 	}
 
 	position = 0;
@@ -38,11 +38,11 @@ char *argstostr(int ac, char **av)
 	/* Copy each argument to the concatenated string with a newline*/
 
 	for (i = 0; i < ac; i++)
-	{		
+	{
 		strcpy(result + position, av[i]);
 		position += strlen(av[i]);
-		result[position++] = '\n';				
+		result[position++] = '\n';
 	}
 
-	return result;
+	return (result);
 }
