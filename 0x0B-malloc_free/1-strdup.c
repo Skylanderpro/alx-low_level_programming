3ca1fbab2d3f7931ff1b2dfc0ab5140496608591
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-27 08:46:49 +0300

    Applied modifications to files: 0x0B-malloc_free/1-main.c 0x0B-malloc_free/1-strdup.c 0x0B-malloc_free/s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strdup - duplicates string in newly allocated space in memory
+ *@str: string duplicated
+ *
+ *Return: a pointer to duplicated string or NULL for insufficient memory or str = NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate = (char *)malloc(strlen(str) + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	strcpy(duplicate, str);
+
+	return (duplicate);
+}
