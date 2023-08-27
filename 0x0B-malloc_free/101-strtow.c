3b211c156b37efd274c0bfed3fb337213c880024
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-27 02:15:53 -0700

    Applied modifications to files: 0x0B-malloc_free/101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,110 +1,101 @@
-#include "main.h"
-/**
- *is_whitespace - checks for whitespaces
- *@c: the character being checked
- *Return: the whitespace character
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int is_whitespace(char c)
 {
-	return c == ' ' || c == '\t' || c == '\n';
+    return c == ' ' || c == '\t' || c == '\n';
 }
-/**
- *count_words - counts the words in the string
- *@str: the string to count words in
- *Return: counted words or
- */
+
 int count_words(char *str)
 {
-	int count = 0;
-	int i = 0;
+    int count = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		/* Skip leading whitespaces*/
-		while (is_whitespace(str[i]))
-		{
-			i++;
-		}
+    while (str[i] != '\0')
+    {
+        // Skip leading whitespaces
+        while (is_whitespace(str[i]))
+        {
+            i++;
+        }
 
-		if (str[i] != '\0') /* Non-whitespace character found*/
-		{
-			count++;
-			/* Skip the word*/
-			while (str[i] != '\0' && !is_whitespace(str[i]))
-			{
-				i++;
-			}
-		}
-	}
-	return (count);
+        if (str[i] != '\0') // Non-whitespace character found
+        {
+            count++;
+
+            // Skip the word
+            while (str[i] != '\0' && !is_whitespace(str[i]))
+            {
+                i++;
+            }
+        }
+    }
+
+    return count;
 }
-/**
- *strtow - converts strings to words
- *@str: the string converted to words
- *Return: the pointer to converted words or NULL
- */
+
 char **strtow(char *str)
 {
-	int num_words, length, start, word_index, i, j;
-	char **words;
+    if (str == NULL || *str == '\0' || count_words(str) == 0)
+    {
+        return NULL;
+    }
 
-	if (str == NULL || *str == '\0')
-	{
-		return (NULL);
-	}
+    int num_words = count_words(str);
 
-	num_words = count_words(str);
+    char **words = (char **)malloc((num_words + 1) * sizeof(char *));
 
-	words = (char **)malloc((num_words + 1) * sizeof(char *));
+    if (words == NULL)
+    {
+        return NULL;
+    }
 
-	if (words == NULL)
-	{
-		return (NULL);
-	}
+    int word_index = 0;
+    int i = 0;
 
-	word_index = 0;
-	i = 0;
+    while (str[i] != '\0')
+    {
+        // Skip leading whitespaces
+        while (is_whitespace(str[i]))
+        {
+            i++;
+        }
 
-	while (str[i] != '\0')
-	{
-		/*Skip leading whitespaces*/
-		while (is_whitespace(str[i]))
-		{
-			i++;
-		}
+        if (str[i] != '\0') // Non-whitespace character found
+        {
+            int start = i;
 
-		if (str[i] != '\0') /*Non-whitespace character found*/
-		{
-			start = i;
-			/*Find the end of the word*/
-			while (str[i] != '\0' && !is_whitespace(str[i]))
-			{
-				i++;
-			}
+            // Find the end of the word
+            while (str[i] != '\0' && !is_whitespace(str[i]))
+            {
+                i++;
+            }
 
-			length = i - start;
-			/*Allocate memory for the word and copy it*/
-			words[word_index] = (char *)malloc((length + 1) * sizeof(char));
+            int length = i - start;
 
-			if (words[word_index] == NULL)
-			{
-				/*Free memory for previously allocated words*/
-				for (j = 0; j < word_index; j++)
-				{
-					free(words[j]);
-				}
-				free(words);
-				return (NULL);
-			}
+            // Allocate memory for the word and copy it
+            words[word_index] = (char *)malloc((length + 1) * sizeof(char));
+            
+            if (words[word_index] == NULL)
+            {
+                // Free memory for previously allocated words
+                for (int j = 0; j < word_index; j++)
+                {
+                    free(words[j]);
+                }
+                free(words);
+                return NULL;
+            }
 
-			strncpy(words[word_index], str + start, length);
-			words[word_index][length] = '\0'; /*Null-terminate the word*/
+            strncpy(words[word_index], str + start, length);
+            words[word_index][length] = '\0'; // Null-terminate the word
+            
+            word_index++;
+        }
+    }
 
-			word_index++;
-		}
-	}
+    words[word_index] = NULL; // Set the last element to NULL
 
-	words[word_index] = NULL; /*Set the last element to NULL*/
-
-	return (words);
+    return words;
 }
