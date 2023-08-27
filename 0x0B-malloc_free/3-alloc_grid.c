4e08c4b301fd4ead009242e4a38e46b2cbbd0bef
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-27 10:47:56 +0300

    Applied modifications to files: 0x0B-malloc_free/3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,13 +13,15 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = (int **)malloc(width * height * sizeof(int *));
+	grid = (int **)malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
+		grid [i] = (int *)malloc(width * sizeof(int));
+
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
