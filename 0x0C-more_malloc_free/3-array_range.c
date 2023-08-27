7a34c8c5b3a04c53a6990a2e42baaa943b65db1f
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-27 14:34:00 +0300

    Applied modifications to files: 0x0C-more_malloc_free/3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,7 +4,8 @@
  * @min: The minimum value.
  * @max: The maximum value.
  *
- * Return: Pointer to the newly created array, or NULL on failure or invalid input.
+ * Return: Pointer to the newly created array, or NULL on
+ * failure or invalid input.
  */
 int *array_range(int min, int max)
 {
@@ -12,16 +13,15 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-        
+
 	size = max - min + 1;
 	arr = malloc(size * sizeof(int));
 
 	if (arr == NULL)
 		return (NULL);
 
-
 	for (i = 0; i < size; i++)
 		arr[i] = min + i;
-		        
+
 	return (arr);
 }
