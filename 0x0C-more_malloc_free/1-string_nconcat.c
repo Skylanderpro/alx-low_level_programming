01640a9b3ee35ad87defed5b55ac939a6235c3c8
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-08-27 14:04:12 +0300

    Applied modifications to files: 0x0C-more_malloc_free/1-main.c 0x0C-more_malloc_free/1-string_nconcat 0x0C-more_malloc_free/1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,18 +11,21 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int len1, len2;
+	char *result;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	if (n >= len2)
 		n = len2;
 
-	char *result = (char *)malloc(len1 + n + 1);
+	result = (char *)malloc(len1 + n + 1);
 	if (result == NULL)
 	{
 		return (NULL);
