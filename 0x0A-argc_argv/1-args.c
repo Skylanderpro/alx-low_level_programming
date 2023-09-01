b3fde0cd21f8301c802ab1c1431dcc600859992b
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-01 19:42:29 +0300

    Applied modifications to files: 0x0A-argc_argv/1-args.c 0x0A-argc_argv/nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - prints the number of arguments to main
+ *@argc: number of arguments to main
+ *@argv: string storing arguments to main
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
