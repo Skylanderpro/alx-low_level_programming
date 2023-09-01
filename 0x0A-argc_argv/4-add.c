43239479413486cffb6f87bd029fe8b233d66745
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-01 20:26:45 +0300

    Applied modifications to files: 0x0A-argc_argv/4-add.c 0x0A-argc_argv/add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ *main - adds arguments to it
+ *@argc: the number of arguments to main
+ *@argv: the string storing the arguments to main
+ *
+ *Return: 1 for error and 0 o.w
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, k, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(argv[i][0]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	for (j = 1; j < argc; j++)
+	{
+		k = atoi(argv[j]);
+		sum += k;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
