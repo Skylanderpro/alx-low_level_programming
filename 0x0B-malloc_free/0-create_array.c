6397d5cad41b2f6961ed3bd4855252bc3453bb0a
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-05 22:17:38 +0300

    Applied modifications to files: 0x0B-malloc_free/0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,17 +6,20 @@
  *Return: the array or NULL for errors
  */
 char *create_array(unsigned int size, char c) {
+	char *array;
+	unsigned int i;
+
 	if (size == 0) {
 		return (NULL);
 	}
 
-	char *array = (char *)malloc(size * sizeof(char));
+	array = (char *)malloc(size * sizeof(char));
 
 	if (array == NULL) {
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++) {
+	for (i = 0; i < size; i++) {
 		array[i] = c;
 	}
 
