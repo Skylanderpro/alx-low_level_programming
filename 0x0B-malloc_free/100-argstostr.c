bf0a53322923a4977f464636a4718488f52644d9
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-05 22:53:29 +0300

    Applied modifications to files: 0x0B-malloc_free/100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,30 +5,37 @@
  *@av: pointer to the arguments
  *Return: pointer to a new string or NULL for error
  */
-char *argstostr(int ac, char **av) {
+char *argstostr(int ac, char **av)
+{
 	int totalLength = 0;
 	int index = 0;
 	char *concatenated;
 	int i;
 
-	if (ac == 0 || av == NULL) {
+	if (ac == 0 || av == NULL)
+	{
 		return (NULL);
 	}
 
-	for (i = 0; i < ac; i++) {
-		if (av[i] != NULL) {
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+		{
 			totalLength += strlen(av[i]) + 1;
 		}
 	}
 
 	concatenated = (char *)malloc(totalLength * sizeof(char) + 1);
 
-	if (concatenated == NULL) {
+	if (concatenated == NULL)
+	{
 		return (NULL);
 	}
 
-	for (i = 0; i < ac; i++) {
-		if (av[i] != NULL) {
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+		{
 			strcpy(concatenated + index, av[i]);
 			index += strlen(av[i]);
 			concatenated[index] = '\n';
