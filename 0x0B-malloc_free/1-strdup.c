f102a78a31ceba4e203e68e93e975e8dc5a4cb99
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-05 22:20:37 +0300

    Applied modifications to files: 0x0B-malloc_free/1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,12 +7,15 @@
  */
 char *_strdup(char *str)
 {
+	char *duplicate;
+	size_t len;
+
 	if (str == NULL)
 		return (NULL);
 
-	size_t len = strlen(str);
+	len = strlen(str);
 
-	char *duplicate = (char *)malloc((len + 1) * sizeof(char));
+	duplicate = (char *)malloc((len + 1) * sizeof(char));
 
 	if (duplicate == NULL)
 		return (NULL);
