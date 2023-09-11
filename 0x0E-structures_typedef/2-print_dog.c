b5a213e9c200bcae68a778335d4ff853dde645d9
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-11 22:45:42 +0300

    Applied modifications to files: 0x0E-structures_typedef/2-main.c 0x0E-structures_typedef/2-print_dog.c 0x0E-structures_typedef/c 0x0E-structures_typedef/dog.h

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,10 @@
  * If the struct itself is NULL, it does nothing.
  */
 void print_dog(struct dog *d) {
-	if (d != NULL) { // Check if the pointer to struct dog is not NULL
+	if (d != NULL)
+	{
 		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-		printf("Age: %.6f\n", d->age); // Use "%.6f" to print with 6 decimal places
+		printf("Age: %.6f\n", d->age);
 		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,5 @@ struct dog
 };
 #include <stdlib.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
