328c1d04ffba0224e757628e34237ef60ec9671a
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-21 06:01:52 +0300

    Applied modifications to files: 0x10-variadic_functions/1-print_numbers.c 0x10-variadic_functions/main.c 0x10-variadic_functions/variadic_functions.h

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,4 @@
-#include <stdarg.h>
-
+#include "variadic_functions.h"
 /**
  * print_numbers - Print numbers followed by a new line.
  * @separator: The string to be printed between numbers (or NULL)
@@ -9,38 +8,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	int num, j, count, temp, k, divisor, digit; 
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		num = va_arg(args, int);
-			temp = num;
-		count = 0;
-		while (temp != 0)
-		{
-			temp /= 10;
-			count++;
-		}
-		for (j = count - 1; j >= 0; j--)
-		{
-			divisor = 1;
-			for (k = 0; k < j; k++)
-				divisor *= 10;
-			digit = num / divisor;
-			_putchar(digit + '0');
-			num -= digit * divisor;
-		}
+		printf("%d", va_arg(args, int));
 
 		if (i < n - 1 && separator != NULL)
-		{
-			while (*separator != '\0')
-				_putchar(*separator++);
-		}
+			printf("%s", separator);
 	}
 
-	_putchar('\n');
+	printf("\n");
 
 	va_end(args);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdarg.h>
+#include <stddef.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
