a124ef2fa98ab314f1617061b723235f1cb3914d
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-22 21:20:00 +0300

    Applied modifications to files: 0x12-singly_linked_lists/3-add_node_end.c 0x12-singly_linked_lists/lists.h

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a linked list
+ * @head: Pointer to a pointer to the head of the list
+ * @str: The string to be duplicated and stored in the new node
+ *
+ * Return: Address of the new element, or (NULL) if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str) {
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL) {
+		fprintf(stderr, "Memory allocation failed.\n");
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL) {
+		fprintf(stderr, "Memory allocation failed for string duplication.\n");
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+
+	if (*head == NULL) {
+		*head = new_node;
+	} else {
+		list_t *current = *head;
+		while (current->next != NULL) {
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+
+	return (new_node);
+}
