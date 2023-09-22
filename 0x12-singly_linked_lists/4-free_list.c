b71618bc6e889840f32af9011e2ee62c31d1d3f4
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-22 21:35:49 +0300

    Applied modifications to files: 0x12-singly_linked_lists/4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,11 +4,13 @@
  * free_list - Frees a linked list
  * @head: Pointer to the head of the list
  */
-void free_list(list_t *head) {
+void free_list(list_t *head)
+{
 	list_t *current = head;
 	list_t *next;
 
-	while (current != NULL) {
+	while (current != NULL)
+	{
 		next = current->next;
 		free(current->str);
 		free(current);
