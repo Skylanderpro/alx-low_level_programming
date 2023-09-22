025c63fd1d52e2a185d33361c96ba212c21a9e3e
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-22 06:15:48 +0300

    Applied modifications to files: 0x12-singly_linked_lists/1-list_len.c 0x12-singly_linked_lists/lists.h

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: Pointer to the head of the list.
+ * Return: The number of elements in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+
+	return node_count;
+}
