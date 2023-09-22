92df9696247d1d65afdb9393143e92b62b4660b6
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-22 21:39:02 +0300

    Applied modifications to files: 0x12-singly_linked_lists/100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_message - Prints the specified message.
+ *
+ * This function is marked with __attribute__((constructor)),
+ * ensuring it is executed before main.
+ */
+void print_message() __attribute__((constructor));
+
+void print_message()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
