504632d953e925bd0fb8f2f9362ce62552ac0b21
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-30 09:56:15 +0300

    Applied modifications to files: 0x14-bit_manipulation/0-binary_to_uint.c 0x14-bit_manipulation/0-main.c 0x14-bit_manipulation/README.md 0x14-bit_manipulation/a 0x14-bit_manipulation/main.h 0x14-bit_manipulation/p

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *binary_to_uint - converts binary to int
+ *@b: the binary to be converted to int
+ *
+ *Return: the unsigned int of b
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	size_t length = 0, i;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[length] != '\0')
+		length++;
+
+	for (i = 0; i < length; i++) {
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		result = (result << 1) + (b[i] - '0');
+	}
+
+	return (result);
+}
