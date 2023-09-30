19faa8a66349c6aec269cc0e7a6790321ecf352b
Joel Okhonjo <joelokhonjo3@example.com>
2023-09-30 10:03:33 +0300

    Applied modifications to files: 0x14-bit_manipulation/1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *print_binary - prints binary representation of a number
+ *@n: number whose binary rep is printed
+ */
+void print_binary(unsigned long int n)
+{
+	int bits = sizeof(n) * 8;
+	unsigned long int mask;
+
+	for (int i = bits - 1; i >= 0; i--)
+	{
+		mask = 1UL << i;
+		printf("%c", (n & mask) ? '1' : '0');
+	}
+
+	printf("\n");
+}
